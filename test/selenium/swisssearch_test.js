98db1362cc431bc6c3854cbd3d1c75521b2e5645
Kevin André <kevin.andre.gx@gmail.com>
2015-05-07 10:46:02 +0200

    Adapt BS tests

--- test/selenium/swisssearch_test.js
+++ test/selenium/swisssearch_test.js
@@ -14,10 +14,6 @@ var runTest = function(cap, driver, target){
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Raron')]"));
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Flugfeld')]")).click();
   driver.findElement(webdriver.By.xpath("//a[contains(@href, '" + QUERYSTRING_OF_RARON + "')]"));
-  //parameter should disappear when selection is done
-  driver.findElement(webdriver.By.xpath("//input[@ng-model='permalinkValue']")).getAttribute("value").then(function(val) {
-    assert.ok(val.indexOf('swisssearch') == -1);
-  });
 
   //swisssearch parameter with multiple results (locations and layers), reset selection
   driver.get(target + '/?swisssearch=wasser&lang=de');
@@ -26,10 +22,6 @@ var runTest = function(cap, driver, target){
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Standorte')]"));
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Karten')]"));
   driver.findElement(webdriver.By.xpath("//i[@ng-click='clearInput()']")).click();
-  //parameter should disappear when selection is done
-  driver.findElement(webdriver.By.xpath("//input[@ng-model='permalinkValue']")).getAttribute("value").then(function(val) {
-    assert.ok(val.indexOf('swisssearch') == -1);
-  });
 
   //swissearch parameter with 1 result (direct selection doesn't work in safari 5.1)
   if (!(cap.browser == 'Safari' && cap.browser_version == '5.1')) {
@@ -37,18 +29,9 @@ var runTest = function(cap, driver, target){
     //wait until topics related stuff is loaded. We know this when catalog is there
     driver.findElement(webdriver.By.xpath("//a[contains(text(), 'Grundlagen und Planung')]"));
     driver.findElement(webdriver.By.xpath("//a[contains(@href, '" + QUERYSTRING_MOOS + "')]"));
-    //parameter stays after initial automatic selection
-    driver.findElement(webdriver.By.xpath("//input[@ng-model='permalinkValue']")).getAttribute("value").then(function(val) {
-      assert.ok(val.indexOf(QUERYSTRING_MOOS) > -1);
-      assert.ok(val.indexOf('swisssearch=br') > -1);
-    });
     //parameter is removed by map action (simulating zoom here)
     driver.findElement(webdriver.By.xpath("//button[@class='ol-zoom-in']")).click();
     driver.findElement(webdriver.By.xpath("//a[contains(@href, 'zoom=11')]"));
-    //parameter should disappear when selection is done
-    driver.findElement(webdriver.By.xpath("//input[@ng-model='permalinkValue']")).getAttribute("value").then(function(val) {
-      assert.ok(val.indexOf('swisssearch') == -1);
-    });
   }
 }
 
